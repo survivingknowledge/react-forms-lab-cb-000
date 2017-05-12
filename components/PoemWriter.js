6c09fee58060c4dffc1876354193cacb594af368
Jordan Sleeper <survivingknowledge@gmail.com>
2017-05-12 14:13:52 -0400

    Done.

--- components/PoemWriter.js
+++ components/PoemWriter.js
@@ -6,7 +6,7 @@ function countWords(line){
 
 function isValidPoem(poem){
   const poemLines = poem.split('\n').filter((l) => l);
-  const isRightAmountOfLines = poemLines.length;
+  const isRightAmountOfLines = poemLines.length === 3;
   const hasRightAmountOfWords = countWords(poemLines[0]) === 5 && countWords(poemLines[1]) === 3 && countWords(poemLines[2]) === 5;
   return isRightAmountOfLines && hasRightAmountOfWords;
 }
