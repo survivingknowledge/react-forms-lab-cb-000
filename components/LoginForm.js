ac3e6baa8e77b659db839207ff9a58febd470f84
Jordan Sleeper <survivingknowledge@gmail.com>
2017-05-12 13:54:51 -0400

    Done.

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,25 +1,62 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export default class LoginForm extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
+    this.handleUser = this.handleUser.bind(this);
+    this.handlePass = this.handlePass.bind(this);
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+  }
+
+  handlePass(ev){
+    this.setState({
+      password: ev.target.value
+    })
+  }
+
+  handleUser(ev){
+    this.setState({
+      username: ev.target.value
+    })
+  }
+
+  //for using multiple targets
+  //handleUser(field, event){
+  // this.setState({ [field]: event.target.value })
+  handleFormSubmit(ev){
+    ev.preventDefault();
+    const {username, password} = this.state;
+
+    if(!username || !password){
+      return;
+    }
+
+    this.props.onSubmit({
+      username,
+      password
+    })
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleFormSubmit}>
         <div>
           <label>
             Username
-            <input id="test-username" type="text" />
+            //can also do onChange={this.handleUser.bind(this, 'username')} to have mutli fields use the same method
+            <input id="test-username" type="text" value={this.state.username} onChange={this.handleUser}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="test-password" type="password" />
+            <input id="test-password" type="password" value={this.state.password} onChange={this.handlePass}/>
           </label>
         </div>
         <div>
@@ -28,4 +65,8 @@ export default class LoginForm extends React.Component {
       </form>
     );
   }
+}
+
+LoginForm.propTypes = {
+  onSubmit: PropTypes.func,
 }
--- components/TwitterMessage.js
+++ components/TwitterMessage.js
@@ -4,14 +4,25 @@ export default class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      value: '',
+    };
+
+    this.maxChars = this.maxChars.bind(this);
+  }
+
+  maxChars(event){
+      this.setState({
+        value: event.target.value,
+      });
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text" value={this.state.value} onChange={this.maxChars}/>
+        <strong>{this.props.maxChars - this.state.value.length} characters remaining</strong>
       </div>
     );
   }
